794f011a24900d66398e301972f426abb8e375d0
retromodernism <43184201+retromodernism@users.noreply.github.com>
2021-06-11 15:29:29 +0800

    Добавил блок Map

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -4,18 +4,6 @@ import {
   GoogleMap,
   Marker,
 } from "react-google-maps";
-import "./index.scss";
-
-// const MyMapComponent = (props) => (
-//   <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-//     {props.isMarkerShown && (
-//       <Marker position={{ lat: -34.397, lng: 150.644 }} />
-//     )}
-//   </GoogleMap>
-// );
-
-// <MyMapComponent isMarkerShown />// Map with a Marker
-// <MyMapComponent isMarkerShown={false} />// Just only Map
 
 const Map = withScriptjs(
   withGoogleMap((props) => {
